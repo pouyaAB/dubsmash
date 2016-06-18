4fff5d3e3ea965ab8bf633c900a55f72af71ce85
Pouya <New@News-MacBook-Pro.local>
2016-06-18 16:36:42 -0700

    first

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-        return 'Hello World! I have been seen times.'
+@app.route('/add-emoji/')
+def add_emoji(emoji=None):
+        return 'add emoji.'
+
+@app.route('/remove-emoji/')
+def remove_emoji(emoji=None):
+        return 'remove emoji.'
+
+@app.route('/get-all-dubs/')
+def get_emojis(dubs=None):
+        return 'get emojies.'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
