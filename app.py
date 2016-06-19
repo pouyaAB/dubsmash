4116211ba754b120fb926647838605bfacebc626
Pouya <New@News-MacBook-Pro.local>
2016-06-19 01:13:25 -0700

    stablebefore last function

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from flask import request
+import time
 import requests
 
 app = Flask(__name__)
+
+@app.route('/remove')
+def remove():
+    r = requests.post('http://172.22.0.4:8182',
+            json = {"gremlin": "g.V().drop().iterate()"})
+    return r.text
+
+@app.route('/sort-dub')
+def sort_dub():
+    r = requests.post('http://172.22.0.4:8182',
+            json = {"gremlin": "g.V().has('dub').inE('react').values('timestamp').max()"})
+    return r.text
 
 @app.route('/add-dub/<dub_name>')
 def add_dub(dub_name):
     if dub_name:
         r = requests.post('http://172.22.0.4:8182',
                 json = {"gremlin": "graph.addVertex('dub', '{}')".format(dub_name)})
-        #return r.text
         r_json =  r.json()
-        return str(r_json['result']['data'][0]['id'])
-    else:
-        return "404"
+        if 'result' in r_json and 'data' in r_json['result'] and (len(r_json['result']['data']) == 1) and\
+                'id' in r_json['result']['data'][0]:
+            return str(r_json['result']['data'][0]['id'])
+        else:
+            return '400'
+    return "404"
 
 @app.route('/add-user/<user_name>')
 def add_user(user_name):
@@ -32,11 +47,14 @@ def react():
     emoji_string = request.args.get('emoji_string')
     if user_name and dub_id and emoji_string:
         check_in_db = requests.post('http://172.22.0.4:8182',
-                json = {"gremlin": "g.V('{}').inE('react').has('emoji', '{}').outV().has('user', '{}').count()".format(dub_id, emoji_string, user_name)})
+                json = {"gremlin": "g.V('{}').inE('react').has('emoji', '{}')\
+                        .outV().has('user', '{}').count()".format(dub_id, emoji_string, user_name)})
         if check_in_db.json()['result']['data'][0] == 0:
+            timestamp = str(time.time())
             r = requests.post('http://172.22.0.4:8182',
                     json = {"gremlin": "v1=g.V('{}').next(); v2=g.V().has('user', '{}').next();\
-                        e1=v2.addEdge('react', v1, 'emoji', '{}')".format(dub_id, user_name, emoji_string)})
+                        e1=v2.addEdge('react', v1, 'emoji', '{}', 'timestamp', '{}')"\
+                        .format(dub_id, user_name, emoji_string, timestamp)})
             return r.text
 
     return "404"
