2672ded97682b8523db6e2bfd0c7213472c12f91
Pouya <New@News-MacBook-Pro.local>
2016-06-19 10:37:04 -0700

    code draft done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, Response
 import time
 import requests
 from operator import itemgetter
@@ -7,16 +6,27 @@ from collections import defaultdict
 
 app = Flask(__name__)
 
+def validate_json(json_obj):
+    if 'message' in json_obj and 'Error encountered evaluating script' in json_obj['message']:
+        return False
+    return True
+
+
 @app.route('/remove')
-def remove():
+def remove_all():
     r = requests.post('http://172.22.0.4:8182',
             json = {"gremlin": "g.V().drop().iterate()"})
+    if not validate_json(r.json()):
+        return Response({}, status=404, mimetype='application/json')
+
     return r.text
 
 @app.route('/parse')
 def parse():
     r = requests.post('http://172.22.0.4:8182',
             json = {"gremlin": "g.V().has('dub').inE('react')"})
+    if not validate_json(r.json()):
+        return Response({}, status=404, mimetype='application/json')
     reacts = r.json()['result']['data']
     #return r.text
     react_dict = {}
@@ -43,34 +53,31 @@ def parse():
     return str(react_return_list)
 
 
-@app.route('/sort-dub')
-def sort_dub():
-    r = requests.post('http://172.22.0.4:8182',
-            json = {"gremlin": "g.V().has('dub').inE('react').values('timestamp')"})
-    return r.text
-
 @app.route('/add-dub/<dub_name>')
 def add_dub(dub_name):
     if dub_name:
         r = requests.post('http://172.22.0.4:8182',
                 json = {"gremlin": "graph.addVertex('dub', '{}')".format(dub_name)})
+        if not validate_json(r.json()):
+            return Response({}, status=404, mimetype='application/json')
         r_json =  r.json()
         if 'result' in r_json and 'data' in r_json['result'] and (len(r_json['result']['data']) == 1) and\
                 'id' in r_json['result']['data'][0]:
             return str(r_json['result']['data'][0]['id'])
         else:
-            return '400'
-    return "404"
+            return Response({}, status=400, mimetype='application/json')
+    return Response({}, status=404, mimetype='application/json')
 
 @app.route('/add-user/<user_name>')
 def add_user(user_name):
     if user_name:
         r = requests.post('http://172.22.0.4:8182',
                 json = {"gremlin": "graph.addVertex('user', '{}')".format(user_name)})
+        if not validate_json(r.json()):
+            return Response({}, status=404, mimetype='application/json')
         r_json =  r.json()
         return str(r_json['result']['data'][0]['id'])
-    else:
-        return "404"
+    return Response({}, status=404, mimetype='application/json')
 
 @app.route('/react', methods=['GET'])
 def react():
@@ -81,20 +88,40 @@ def react():
         check_in_db = requests.post('http://172.22.0.4:8182',
                 json = {"gremlin": "g.V('{}').inE('react').has('emoji', '{}')\
                         .outV().has('user', '{}').count()".format(dub_id, emoji_string, user_name)})
+        if not validate_json(check_in_db.json()):
+            return Response({}, status=404, mimetype='application/json')
         if check_in_db.json()['result']['data'][0] == 0:
             timestamp = str(time.time())
             r = requests.post('http://172.22.0.4:8182',
                     json = {"gremlin": "v1=g.V('{}').next(); v2=g.V().has('user', '{}').next();\
                         e1=v2.addEdge('react', v1, 'emoji', '{}', 'timestamp', '{}')"\
                         .format(dub_id, user_name, emoji_string, timestamp)})
+            if not validate_json(r.json()):
+                return Response({}, status=404, mimetype='application/json')
             return r.text
 
-    return "404"
+    return Response({}, status=404, mimetype='application/json')
+
+@app.route('/remove-react', methods=['GET'])
+def remove_react():
+    dub_id = request.args.get('dub_id')
+    user_name = request.args.get('user_name')
+    emoji_string = request.args.get('emoji_string')
+    if user_name and dub_id and emoji_string:
+        check_in_db = requests.post('http://172.22.0.4:8182',
+                json = {"gremlin": "g.V('{}').inE('react').has('emoji', '{}')\
+                        .outV().has('user', '{}').outE().has('emoji', '{}').drop()".format(dub_id, emoji_string, user_name, emoji_string)})
+        if not validate_json(check_in_db.json()):
+            return Response({}, status=404, mimetype='application/json')
+        return check_in_db.text
+    return Response({}, status=404, mimetype='application/json')
 
 @app.route('/all-dubs/')
 def list_all_dubs():
     r = requests.post('http://172.22.0.4:8182',
             json = {"gremlin": "g.V()"})
+    if not validate_json(r.json()):
+        return Response({}, status=404, mimetype='application/json')
     r_json =  r.json()
     dubs = {}
     #return str(r.text)
@@ -111,6 +138,8 @@ def list_all_dubs():
 def list_all_users():
     r = requests.post('http://172.22.0.4:8182',
             json = {"gremlin": "g.V()"})
+    if not validate_json(r.json()):
+        return Response({}, status=404, mimetype='application/json')
     r_json =  r.json()
     users = {}
     #return str(r.text)
@@ -122,20 +151,6 @@ def list_all_users():
                 users[user['id']] = user_obj['user'][0]['value']
 
     return str(users)
-
-
-@app.route('/add-emoji/')
-def add_emoji(emoji=None):
-    return requests.post('titan:8182',
-            json = {"gremlin": "graph.addvertex({}:)"})
-
-@app.route('/remove-emoji/')
-def remove_emoji(emoji=None):
-        return 'remove emoji.'
-
-@app.route('/get-all-dubs/')
-def get_emojis(dubs=None):
-        return 'get emojies.'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
